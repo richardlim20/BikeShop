47f52e38d45c317419d59d8c2b552fde56d9f9ac
Richard Lim <s3901013@student.rmit.edu.au>
2024-09-19 14:57:05 +1000

    update

--- paypalConfig.php
+++ paypalConfig.php
@@ -7,9 +7,9 @@
 define('PAYPAL_ID', 'mango__82@hotmail.com'); //seller email
 define('PAYPAL_SANDBOX', TRUE); //TRUE or FALSE
 //redirect page
-define('PAYPAL_RETURN_URL', 'http://localhost/a2-q1/success.php'); 
-define('PAYPAL_CANCEL_URL', 'http://localhost/a2-q1/cancel.php'); 
-define('PAYPAL_NOTIFY_URL', 'http://127.0.0.1/a2-q1/ipn.php');
+define('PAYPAL_RETURN_URL', 'http://localhost/secure-a2-q1/success.php'); 
+define('PAYPAL_CANCEL_URL', 'http://localhost/secure-a2-q1/cancel.php'); 
+define('PAYPAL_NOTIFY_URL', 'http://127.0.0.1/secure-a2-q1/ipn.php');
 //define currency
 define('PAYPAL_CURRENCY', 'AUD');
 
